bef1b8725fd1b31d9cd3931c5353e04236f6812a
Rishabh Pareek <rishabh.pareek21@st.niituniversity.in>
2024-06-21 16:28:39 +0530

    Updates

---
+++ precache-manifest.f945252acf9d486a0fac55ae6055811c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4237974964ffe89832b8",
+    "url": "/Digital-Portfolio/static/css/main.4ee3f80c.chunk.css"
+  },
+  {
+    "revision": "4237974964ffe89832b8",
+    "url": "/Digital-Portfolio/static/js/main.b7e0e84a.chunk.js"
+  },
+  {
+    "revision": "7f167e094db4da6c1bea",
+    "url": "/Digital-Portfolio/static/js/runtime~main.e99fc450.js"
+  },
+  {
+    "revision": "4ef8a17b0f2d666056e4",
+    "url": "/Digital-Portfolio/static/css/2.29582032.chunk.css"
+  },
+  {
+    "revision": "4ef8a17b0f2d666056e4",
+    "url": "/Digital-Portfolio/static/js/2.902ad038.chunk.js"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/Digital-Portfolio/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "fed0850a36ae8cfbe9ceaa1ce970c5cc",
+    "url": "/Digital-Portfolio/static/media/graph_embeddings.fed0850a.png"
+  },
+  {
+    "revision": "9c0f986795edbdc96c99bc8853580310",
+    "url": "/Digital-Portfolio/static/media/kafkaml.9c0f9867.png"
+  },
+  {
+    "revision": "4b632d51a45152af9b70077a3d4955ca",
+    "url": "/Digital-Portfolio/static/media/portfolio.4b632d51.png"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/Digital-Portfolio/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "48efb0848ecfc3f502a817982144c30c",
+    "url": "/Digital-Portfolio/index.html"
+  }
+];
