4b6f3168ea582cc80cc43b55d28ea53376616a3b
Rishabh Pareek <rishabh.pareek21@st.niituniversity.in>
2024-06-21 17:09:29 +0530

    router modified

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
 */
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
