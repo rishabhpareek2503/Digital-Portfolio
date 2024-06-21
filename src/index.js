9f44e967914785524af3add18cfe71ba84ad0d35
Rishabh Pareek <rishabh.pareek21@st.niituniversity.in>
2024-06-21 17:53:39 +0530

    index.html  modified

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/*
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -10,18 +10,4 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-*/
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { HashRouter } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <HashRouter basename={process.env.PUBLIC_URL}>
-        <App />
-    </HashRouter>
 );
