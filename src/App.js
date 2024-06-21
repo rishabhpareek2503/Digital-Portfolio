f60bb7e6a3f3efe317b434d8443b5c66d47c9540
Rishabh Pareek <rishabh.pareek21@st.niituniversity.in>
2024-06-21 16:39:38 +0530

    Router Changer

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ReactGA from 'react-ga';
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects";
 import Skills from "./pages/Skills/Skills";
-import Blogs from "./pages/Blogs"
+import Blogs from "./pages/Blogs";
 import ScrollToTop from "./components/ScrollToTop";
 import Footer from "./components/Footer/Footer";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 
-import React from 'react'
-
+// Initialize Google Analytics if tracking ID is defined
 if (typeof process.env.REACT_APP_TRACKING_ID !== 'undefined') {
-    ReactGA.initialize(process.env.REACT_APP_TRACKING_ID)
+    ReactGA.initialize(process.env.REACT_APP_TRACKING_ID);
 }
-
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <NavBar/>
+                <NavBar />
                 <br />
-                <ScrollToTop/>
+                <ScrollToTop />
                 <Routes>
-                    <Route path={"/portfolio"} exact element={<Home/>}/>
-                    <Route path={"/projects"} exact element={<Projects/>}/>
-                    <Route path={"/blogs"} exact element={<Blogs/>}/>
-                    <Route path={"/skills"} exact element={<Skills/>}/>
+                    <Route path={"/portfolio"} element={<Home />} />
+                    <Route path={"/projects"} element={<Projects />} />
+                    <Route path={"/blogs"} element={<Blogs />} />
+                    <Route path={"/skills"} element={<Skills />} />
                 </Routes>
-                <Footer/>
+                <Footer />
             </div>
         </Router>
-
-    )
-
+    );
 }
 
 export default App;
