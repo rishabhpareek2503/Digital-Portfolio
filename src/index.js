99f94784776a6b186fa0a3a076a502240cab49e2
Rishabh Pareek <rishabh.pareek21@st.niituniversity.in>
2024-06-21 17:08:27 +0530

    router modified

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -9,4 +10,18 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <HashRouter>
+        <App />
+    </HashRouter>
 );
