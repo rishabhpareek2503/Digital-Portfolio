8dd9b0a945ca4884c80a1bc9319f4ca8111d96bf
Rishabh Pareek <rishabh.pareek21@st.niituniversity.in>
2024-06-26 11:18:56 +0530

    Updates

---
+++ precache-manifest.f3e74b74adfc5b23991b8a740356dca6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "728e75e030642393903e",
+    "url": "/Digital-Portfolio/static/css/main.09fabf5d.chunk.css"
+  },
+  {
+    "revision": "728e75e030642393903e",
+    "url": "/Digital-Portfolio/static/js/main.1b43f0a3.chunk.js"
+  },
+  {
+    "revision": "7f167e094db4da6c1bea",
+    "url": "/Digital-Portfolio/static/js/runtime~main.e99fc450.js"
+  },
+  {
+    "revision": "5bc669f54e98e15c6a05",
+    "url": "/Digital-Portfolio/static/css/2.09f63bee.chunk.css"
+  },
+  {
+    "revision": "5bc669f54e98e15c6a05",
+    "url": "/Digital-Portfolio/static/js/2.e0d3af60.chunk.js"
+  },
+  {
+    "revision": "590aa0c8f484c501ec7d1e74aeb2f751",
+    "url": "/Digital-Portfolio/static/media/myImage.590aa0c8.jpeg"
+  },
+  {
+    "revision": "c170fe666a84d9b2df58b0fe138ad0d9",
+    "url": "/Digital-Portfolio/static/media/Parksense.c170fe66.PNG"
+  },
+  {
+    "revision": "2c6dfea1f4bbba0f1777fd9721c317b0",
+    "url": "/Digital-Portfolio/static/media/CASSIR.2c6dfea1.PNG"
+  },
+  {
+    "revision": "aa5bfc107883a1da4a90795d0d48ed13",
+    "url": "/Digital-Portfolio/static/media/RishabhPortfolio.aa5bfc10.PNG"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/Digital-Portfolio/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/Digital-Portfolio/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "b518c32cf0196191ab87b5dedeaa718e",
+    "url": "/Digital-Portfolio/index.html"
+  }
+];
